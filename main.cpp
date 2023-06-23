be0dc8bb064e1affa173720ad28f9ee7a85bc2ed
andrew <abodrogai@gmail.com>
2023-06-23 18:32:42 +0200

    add outline

--- main.cpp
+++ main.cpp
@@ -6,8 +6,14 @@
 
 int main() {
     printf("Hello from TensorFlow C library version %s\n", TF_Version());
+    // IO reading
+
+    // data processing
     SegFilter segfilter;
     segfilter.doProcessing();
     segfilter.doDecision();
+    // image compression
+
+    // IO writing
     return 0;
 }
