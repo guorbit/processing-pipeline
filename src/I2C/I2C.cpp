2a40d402207bc6bee6b2860b121ab4c158939406
Sean Andrew <2470451a@student.gla.ac.uk>
2023-07-19 20:49:18 +0100

    Added functions to check if file exists and recieve data

--- src/I2C/I2C.cpp
+++ src/I2C/I2C.cpp
@@ -1,14 +1,45 @@
 #include "I2C.hpp"
-#include <fcntl.h>
+#include <sys/ioctl.h> //Functions for controlling system requests 
+#include <linux/i2c-dev.h> //Built in linux kernel functions for an I2C bus
+#include <fcntl.h> //I/O functions for compatibility with ioctl.h
+#include <iostream>
+
 
 I2CBUS::I2CBUS() //Declaration of constructor
 {
-    I2CBUS::*jetson_bus = "/dev/i2c-1"
-    I2CBUS::address = 0x64
-    I2CBUS::image
+    I2CBUS::*jetson_bus = "/dev/i2c-1"; //Location of bus pin 
+    I2CBUS::address = 0x64; //Slave address of Jetson nano
+    I2CBUS::image_file; 
 }
 
 I2CBUS::~I2CBUS() //Declaration of Destructor
+{
+    close(image_file); //Closes file 
+}
+
+void I2CBUS::check_image() //Check that the image file has been opened 
+{
+    if ((image_file = open(jetson_bus, 0_RDWR)) < 0) { //Opening the bus location for Reading and Writing 
+        printf("Failed to open jetson bus \n"); //Print statement for failure to open
+    }
+    else {
+        printf("Located jetson bus \n"); //Bus has been located 
+    }
+}
+
+void I2CBUS::set_slave()  //
+{
+    if (ioctl(image_file, I2C_SLAVE, address)) {
+        printf("Failed to access slave. \n");
+    }
+}
+
+void I2CBUS::read_data()
+{
+
+}
+
+void I2CBUS::write_data()
 {
     
 }
--- src/I2C/I2C.hpp
+++ src/I2C/I2C.hpp
@@ -1,23 +1,23 @@
 
 #include <stdio.h>
-#include <sys/ioctl.h> //Functions for controlling system requests 
-#include <linux/i2c-dev.h> //Built in linux kernel functions for an I2C bus
-#include <fcntl.h>
-#include <iostream>
 
-#ifndef I2C_HPP
+#ifndef I2C_HPP //Define safeguards
 #define I2C_HPP
 
 class I2CBUS{
+
 private:
     char *jetson_bus; //Bus number dependent on pin
     unsigned char address; //Slave address 8 bit length
     int image_file; //Variable for data 
+
 public:
     I2C(); //Constructor
     ~I2C(); //Destructor
-    Check_Image(); //Ensure Image is accessible 
-
+    void check_image(); //Ensure Image is accessible 
+    void set_slave(); //Point driver to jetson slave
+    void read_data(); //Read data from jetson nano
+    void write_data(); //Write data to jetson nano
 };
 
 #endif
