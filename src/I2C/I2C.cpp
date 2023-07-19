344e568682412e084e9ee90e425398f46095e9d7
Andras Bodrogai <abodrogai@gmail.com>
2023-07-19 21:57:50 +0100

    adds dummy compiler to test I2C code
    Co-authored-by: SeanAndrew010601 <SeanAndrew010601@users.noreply.github.com>

--- src/I2C/I2C.cpp
+++ src/I2C/I2C.cpp
@@ -1,13 +1,11 @@
 #include "I2C.hpp"
-#include <sys/ioctl.h> //Functions for controlling system requests 
-#include <linux/i2c-dev.h> //Built in linux kernel functions for an I2C bus
-#include <fcntl.h> //I/O functions for compatibility with ioctl.h
-#include <iostream>
+
+
 
 
 I2CBUS::I2CBUS() //Declaration of constructor
 {
-    I2CBUS::*jetson_bus = "/dev/i2c-1"; //Location of bus pin 
+    I2CBUS::jetson_bus = "/dev/i2c-1"; //Location of bus pin 
     I2CBUS::address = 0x64; //Slave address of Jetson nano
     I2CBUS::image_file; 
 }
@@ -19,7 +17,7 @@ I2CBUS::~I2CBUS() //Declaration of Destructor
 
 void I2CBUS::check_image() //Check that the image file has been opened 
 {
-    if ((image_file = open(jetson_bus, 0_RDWR)) < 0) { //Opening the bus location for Reading and Writing 
+    if ((image_file = open(jetson_bus.c_str(), O_RDWR)) < 0) { //Opening the bus location for Reading and Writing 
         printf("Failed to open jetson bus \n"); //Print statement for failure to open
     }
     else {
@@ -42,4 +40,11 @@ void I2CBUS::read_data()
 void I2CBUS::write_data()
 {
     
+}
+
+int main(){
+    I2CBUS * bus = new I2CBUS();
+
+    delete bus;
+    return 0;
 }
--- src/I2C/I2C.hpp
+++ src/I2C/I2C.hpp
@@ -1,5 +1,11 @@
 
 #include <stdio.h>
+#include <string>
+#include <sys/ioctl.h> //Functions for controlling system requests 
+#include <linux/i2c-dev.h> //Built in linux kernel functions for an I2C bus
+#include <fcntl.h> //I/O functions for compatibility with ioctl.h
+#include <iostream>
+#include <unistd.h> //Constants and types for system calls
 
 #ifndef I2C_HPP //Define safeguards
 #define I2C_HPP
@@ -7,13 +13,13 @@
 class I2CBUS{
 
 private:
-    char *jetson_bus; //Bus number dependent on pin
+    std::string jetson_bus; //Bus number dependent on pin
     unsigned char address; //Slave address 8 bit length
     int image_file; //Variable for data 
 
 public:
-    I2C(); //Constructor
-    ~I2C(); //Destructor
+    I2CBUS(); //Constructor
+    ~I2CBUS(); //Destructor
     void check_image(); //Ensure Image is accessible 
     void set_slave(); //Point driver to jetson slave
     void read_data(); //Read data from jetson nano
