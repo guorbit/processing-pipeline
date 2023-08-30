f830c3ee39813e32922ff0fe7d54cc3570daf635
Andras Bodrogai <abodrogai@gmail.com>
2023-08-30 18:57:48 +0100

    Updates the threadsafety of statemanager state change and status code requests

--- src/state/StateManager.cpp
+++ src/state/StateManager.cpp
@@ -1,88 +1,109 @@
 #include "StateManager.hpp"
 
-
-StateManager::StateManager(ThreadLogger * logger) {
+StateManager::StateManager(ThreadLogger *logger)
+{
     StateManager::logger = logger;
     StateManager::stateMutex = new std::mutex();
     StateManager::state = nullptr;
-    StateManager::logger -> log("State manager initialized...");
+    StateManager::logger->log("State manager initialized...");
     StateManager::transitioned = false;
     StateManager::shutdownQueue = new std::queue<std::function<void()>>();
 }
 
-StateManager::~StateManager() {
+StateManager::~StateManager()
+{
     delete StateManager::state;
     delete StateManager::stateMutex;
-    if (StateManager::requestedState != nullptr){
+    if (StateManager::requestedState != nullptr)
+    {
         delete StateManager::requestedState;
     }
     delete StateManager::shutdownQueue;
 }
 
-IState * StateManager::getState() {
+IState *StateManager::getState()
+{
     return StateManager::state;
 }
 
-IState * StateManager::getRequestedState() {
+IState *StateManager::getRequestedState()
+{
     std::lock_guard<std::mutex> guard(*StateManager::stateMutex);
     return StateManager::requestedState;
 }
 
-void StateManager::setTransitionState(bool transitioned){
+void StateManager::setTransitionState(bool transitioned)
+{
     std::lock_guard<std::mutex> guard(*StateManager::stateMutex);
     StateManager::transitioned = transitioned;
 }
-bool StateManager::getTransitionState(){
+bool StateManager::getTransitionState()
+{
     std::lock_guard<std::mutex> guard(*StateManager::stateMutex);
     return StateManager::transitioned;
 }
 
-int StateManager::getStateCode(){
+int StateManager::getStateCode()
+{
+    std::lock_guard<std::mutex> guard(*StateManager::stateMutex);
+
+    if (StateManager::state == nullptr)
+    {
+        return -1;
+    }
     return StateManager::state->getStateCode();
 }
 
-void StateManager::requestState(IState * state) {
-    // sets requested state    
+void StateManager::requestState(IState *state)
+{
+    // sets requested state
     std::lock_guard<std::mutex> guard(*StateManager::stateMutex);
     StateManager::requestedState = state;
-    
 }
 
-void StateManager::transitionTo(IState * state) {
+void StateManager::transitionTo(IState *state)
+{
     // frees previous state
-    if (state == nullptr){
+    std::lock_guard<std::mutex> guard(*StateManager::stateMutex);
+    if (state == nullptr)
+    {
         LoggingLevelWrapper level(LoggingLevel::ERROR);
-        StateManager::logger -> log(level,"cannot transition to null state!");
+        StateManager::logger->log(level, "cannot transition to null state!");
         return;
     }
-    if (StateManager::state != nullptr){
+    if (StateManager::state != nullptr)
+    {
         delete StateManager::state;
     }
-    StateManager::logger -> log("Transitioning to %s" , state -> getName().c_str());
-
+    StateManager::logger->log("Transitioning to %s", state->getName().c_str());
     // sets new state
     StateManager::state = state;
-    StateManager::state -> setLogger(StateManager::logger);
-   if (StateManager::state -> getName() == "StopState"){
+    StateManager::state->setLogger(StateManager::logger);
+
+    if (StateManager::state->getName() == "StopState")
+    {
         StateManager::shutdown();
     }
-    
 }
 
-void StateManager::runStateProcess() {
+void StateManager::runStateProcess()
+{
     // runs state process
-    StateManager::state -> runStateProcess();
+
+    StateManager::state->runStateProcess();
 }
 
-void StateManager::pushShutdown(std::function<void()> shutdownFunction){
+void StateManager::pushShutdown(std::function<void()> shutdownFunction)
+{
     std::lock_guard<std::mutex> guard(*StateManager::stateMutex);
-    StateManager::shutdownQueue -> push(shutdownFunction);
+    StateManager::shutdownQueue->push(shutdownFunction);
 }
 
-void StateManager::shutdown(){
-    std::lock_guard<std::mutex> guard(*StateManager::stateMutex);
-    while (!StateManager::shutdownQueue -> empty()){
-        StateManager::shutdownQueue -> front()();
-        StateManager::shutdownQueue -> pop();
+void StateManager::shutdown()
+{
+    while (!StateManager::shutdownQueue->empty())
+    {
+        StateManager::shutdownQueue->front()();
+        StateManager::shutdownQueue->pop();
     }
 }
